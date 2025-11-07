WSEI-asp-net/lab3-starter

Add starter project

lab3/Controllers/BlogController.cs
@@ -1,0 +1,52 @@
+using aspNetMVC.Models;
+using aspNetMVC.Models.Entities;
+using aspNetMVC.Models.Mappings;
+using lab3;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspNetMVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var entities = InMemoryBlogStore.GetAll();
+            var viewModels = entities.Select(e => e.ToViewModel()); 
+            return View(viewModels);
+        }
+
+        public IActionResult Article(string id)
+        {
+            var post = InMemoryBlogStore.GetById(id);
+            if (post == null) return NotFound();
+            var viewModel = post.ToViewModel();
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreateBlogArticleModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateBlogArticleModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            if (InMemoryBlogStore.Exists(viewModel.Id))
+            {
+                ModelState.AddModelError(nameof(viewModel.Id), "Id must be unique.");
+                return View(viewModel);
+            }
+
+            var entity = viewModel.ToEntity();
+            InMemoryBlogStore.Add(entity);
+            return RedirectToAction(nameof(Article), new { id = entity.Id });
+        }
+    }
+}

lab3/Controllers/HomeController.cs
@@ -1,0 +1,19 @@
+using System.Diagnostics;
+using aspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

lab3/InMemoryBlogStore.cs
@@ -1,0 +1,36 @@
+using aspNetMVC.Models.Entities;
+
+namespace lab3
+{
+    public static class InMemoryBlogStore
+    {
+        private static readonly List<BlogArticle> Articles = [
+            new() {
+                Id = "welcome", Title = "Welcome to My Blog", Description = "Intro post about the blog.",
+                Content = "<p>This is the first post welcoming users to the blog demo.</p>"
+            },
+            new() {
+                Id = "aspnet-mvc", Title = "Learning ASP.NET MVC", Description = "Basics of ASP.NET MVC.",
+                Content = "<p>In this post we explore controllers, views, and models.</p>"
+            },
+            new() {
+                Id = "routing", Title = "Understanding Routing", Description = "How routing works.",
+                Content =
+                    "<p>Routing maps URLs to controller actions. You can use attribute routing or conventional routing.</p>"
+            }
+        ];
+
+        public static IEnumerable<BlogArticle> GetAll() => Articles;
+
+        public static BlogArticle? GetById(string id) =>
+            Articles.FirstOrDefault(p => p.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+
+        public static void Add(BlogArticle article)
+        {
+            Articles.Add(article);
+        }
+
+        public static bool Exists(string id) =>
+            Articles.Any(a => a.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+    }
+}

lab3/Models/BlogArticleViewModel.cs
@@ -1,0 +1,10 @@
+﻿namespace aspNetMVC.Models
+{
+    public class BlogArticleViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}

lab3/Models/CreateBlogArticleModel.cs
@@ -1,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace aspNetMVC.Models
+{
+    public class CreateBlogArticleModel
+    {
+        [Required]
+        [RegularExpression("^[a-z0-9-]+$")]
+        [StringLength(64)]
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(120)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(240)]
+        public string? Description { get; set; }
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+    }
+}

lab3/Models/Entities/BlogArticle.cs
@@ -1,0 +1,10 @@
+namespace aspNetMVC.Models.Entities
+{
+    public class BlogArticle
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}

lab3/Models/ErrorViewModel.cs
@@ -1,0 +1,9 @@
+namespace aspNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

lab3/Models/Mappings/BlogMappings.cs
@@ -1,0 +1,21 @@
+using aspNetMVC.Models.Entities;
+
+namespace aspNetMVC.Models.Mappings
+{
+    public static class BlogMappings
+    {
+        public static BlogArticleViewModel ToViewModel(this BlogArticle blogArticle) => new() {
+            Id = blogArticle.Id,
+            Title = blogArticle.Title,
+            Description = blogArticle.Description,
+            Content = blogArticle.Content
+        };
+
+        public static BlogArticle ToEntity(this CreateBlogArticleModel blogArticle) => new() {
+            Id = blogArticle.Id,
+            Title = blogArticle.Title,
+            Description = blogArticle.Description ?? string.Empty,
+            Content = blogArticle.Content
+        };
+    }
+}

ButenkoViktor/LAB3ASP.NET-starter

lab3/Controllers/BlogController.cs
@@ -1,0 +1,50 @@
+using lab3.Models;
+using lab3.Models.Mappings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab3.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var entities = InMemoryBlogStore.GetAll();
+            var viewModels = entities.Select(e => e.ToViewModel()); 
+            return View(viewModels);
+        }
+
+        public IActionResult Article(string id)
+        {
+            var post = InMemoryBlogStore.GetById(id);
+            if (post == null) return NotFound();
+            var viewModel = post.ToViewModel();
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreateBlogArticleModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateBlogArticleModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            if (InMemoryBlogStore.Exists(viewModel.Id))
+            {
+                ModelState.AddModelError(nameof(viewModel.Id), "Id must be unique.");
+                return View(viewModel);
+            }
+
+            var entity = viewModel.ToEntity();
+            InMemoryBlogStore.Add(entity);
+            return RedirectToAction(nameof(Article), new { id = entity.Id });
+        }
+    }
+}

lab3/Controllers/HomeController.cs
@@ -1,0 +1,19 @@
+using System.Diagnostics;
+using lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

lab3/InMemoryBlogStore.cs
@@ -1,0 +1,36 @@
+using lab3.Models.Entities;
+
+namespace lab3
+{
+    public static class InMemoryBlogStore
+    {
+        private static readonly List<BlogArticle> Articles = [
+            new() {
+                Id = "welcome", Title = "Welcome to My Blog", Description = "Intro post about the blog.",
+                Content = "<p>This is the first post welcoming users to the blog demo.</p>"
+            },
+            new() {
+                Id = "aspnet-mvc", Title = "Learning ASP.NET MVC", Description = "Basics of ASP.NET MVC.",
+                Content = "<p>In this post we explore controllers, views, and models.</p>"
+            },
+            new() {
+                Id = "routing", Title = "Understanding Routing", Description = "How routing works.",
+                Content =
+                    "<p>Routing maps URLs to controller actions. You can use attribute routing or conventional routing.</p>"
+            }
+        ];
+
+        public static IEnumerable<BlogArticle> GetAll() => Articles;
+
+        public static BlogArticle? GetById(string id) =>
+            Articles.FirstOrDefault(p => p.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+
+        public static void Add(BlogArticle article)
+        {
+            Articles.Add(article);
+        }
+
+        public static bool Exists(string id) =>
+            Articles.Any(a => a.Id.Equals(id, StringComparison.OrdinalIgnoreCase));
+    }
+}

lab3/Models/BlogArticleViewModel.cs
@@ -1,0 +1,10 @@
+﻿namespace lab3.Models
+{
+    public class BlogArticleViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}

lab3/Models/CreateBlogArticleModel.cs
@@ -1,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace lab3.Models
+{
+    public class CreateBlogArticleModel
+    {
+        [Required]
+        [RegularExpression("^[a-z0-9-]+$")]
+        [StringLength(64)]
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(120)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(240)]
+        public string? Description { get; set; }
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+    }
+}

lab3/Models/Entities/BlogArticle.cs
@@ -1,0 +1,10 @@
+namespace lab3.Models.Entities
+{
+    public class BlogArticle
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}

lab3/Models/ErrorViewModel.cs
@@ -1,0 +1,9 @@
+namespace lab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

lab3/Models/Mappings/BlogMappings.cs
@@ -1,0 +1,21 @@
+using lab3.Models.Entities;
+
+namespace lab3.Models.Mappings
+{
+    public static class BlogMappings
+    {
+        public static BlogArticleViewModel ToViewModel(this BlogArticle blogArticle) => new() {
+            Id = blogArticle.Id,
+            Title = blogArticle.Title,
+            Description = blogArticle.Description,
+            Content = blogArticle.Content
+        };
+
+        public static BlogArticle ToEntity(this CreateBlogArticleModel blogArticle) => new() {
+            Id = blogArticle.Id,
+            Title = blogArticle.Title,
+            Description = blogArticle.Description ?? string.Empty,
+            Content = blogArticle.Content
+        };
+    }
+}